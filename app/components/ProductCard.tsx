c3460190aa588e12e6917146138b8e2afaa9da55
David Olowomeye <olowomeyedavid242@gmail.com>
2024-05-21 03:22:21 +0100

    Testing done

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddToCart from './AddToCart';
+
+const ProductCard = () => {
+    return (
+        <div className='p-3 my-5 bg-red-300'>
+            <AddToCart />
+        </div>
+    )
+}
+
+export default ProductCard
